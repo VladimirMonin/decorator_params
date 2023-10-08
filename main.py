0f9fef6a9b91aa6d8df3df8352643b422cd6da15
V <v@yandex.ru>
2023-10-08 17:08:23 +0600

    feat: Функция приветствия теперь принимает аргумент имя

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Решение домашней работы №20
 
-def say_hello():
-    print("Hello, World!")
+def say_hello(name: str):
+    print(f"Hello, {name}!")
 
 
 def main():
-    say_hello()
+    name = input("Enter your name: ")
+    say_hello(name)
+    input("Press Enter to continue...")
+
+
+if __name__ == "__main__":
+    main()
